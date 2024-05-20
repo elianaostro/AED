630601e2845166c96e510931117b3505b8114f6b
Eliana Ostro <elianaostro@gmail.com>
2024-05-20 10:11:30 -0300

    feat: Add function to calculate sum of elements in a list

---
+++ ejercicios/ej14-sumacero.c
@@ -0,0 +1,11 @@
+int* sumacero(void* lista){
+    /* 
+    hago un hash de la lista, con key igual al valor de la lista y value el indice
+    hasta que me quede el hash vacio
+        si el 0 esta esta en el hash 
+            devuelvo un array desde el indice, hasta la cantidade de iteraciones
+        si no
+            a cada elemento del hash le sumo el valor del siguiente elemento de la lista, y borro el ultimo
+    devuelvo NULL
+    */
+}
